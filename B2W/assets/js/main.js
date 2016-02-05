298c4ff947c742f74ec8a0b62dca682452df2fe2
udoyen <datameshprojects@gmail.com>
2016-02-05 17:26:42 +0100

    parallax design near done

---
+++ B2W/assets/js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+  // Cache window object
+  var $window = $(window);
+
+  //parallax background  effect
+  $('section[data-type="background"]').each(function(){
+    var $bgobj = $(this); // assiging the object
+
+    $(window).scroll(function(){
+      //scroll the background at a var speed
+      //the yPos is a negative value because we scrolling it up
+
+      var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+
+      //put together our final background position
+      var coords = '50% '+ yPos + 'px';
+
+      //move the background
+      $bgobj.css({
+         backgroundPosition: coords
+       });// end window scroll
+  });
+});
